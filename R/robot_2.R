f2bd0708fb8a5d3dfdd95b7a9a1e7436fd3f06c4
potockan <potockan@student.mini.pw.edu.pl>
2014-11-14 19:01:01 +0100

    robot2

--- R/robot_2.R
+++ R/robot_2.R
@@ -5,8 +5,9 @@ library(stringi)
 download.file("http://dumps.wikimedia.org/plwiki/latest/plwiki-latest-pages-meta-current.xml.bz2", 
                 "./Data/wiki.xml.bz2")
 
-
-
+# untar("./Data/wiki.xml.bz2", compressed = "bzip2")
+# bzfile("./Data//wiki.xml.bz2")
+#unziped manually
 
 
 

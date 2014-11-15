f53726a07203050e91e69b73073565cca4f2ed8f
potockan <potockan@student.mini.pw.edu.pl>
2014-11-16 00:00:12 +0100

    trying to process xml

--- R/xml_processing.R
+++ R/xml_processing.R
@@ -33,6 +33,10 @@ df2 <- xmlToDataFrame(xmlfile, nodes=list(c("page", pagenodes)))
 getNodeSet(xmlfile, "./Data//plwiki-20141102-pages-articles1-1000linesCW.xml")
 
 
-
+data <- xmlParse("http://forecast.weather.gov/MapClick.php?lat=29.803&lon=-82.411&FcstType=digitalDWML")
+xmlToDataFrame(nodes=getNodeSet(data1,"//data"))[c("location","time-layout")]
+step1 <- xmlToDataFrame(nodes=getNodeSet(data1,"//location/point"))[c("latitude","longitude")]
+step2 <- xmlToDataFrame(nodes=getNodeSet(data1,"//time-layout/start-valid-time"))
+step3 <- xmlToDataFrame(nodes=getNodeSet(data1,"//parameters/temperature"))[c("type="hourly"")]
 
 

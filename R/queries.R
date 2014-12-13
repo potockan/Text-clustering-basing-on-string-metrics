78795b7f6356494634ac41dfbab52825e80c284b
potockan <potockan@student.mini.pw.edu.pl>
2014-12-13 18:00:39 +0100

    analysis again

--- R/queries.R
+++ R/queries.R
@@ -5,20 +5,26 @@ library(stringi)
 
 
 conn <- dbConnect(SQLite(), dbname = "./Data/DataBase/wiki_raw.sqlite")
-aa[20,3]
+
 aa <-
 dbGetQuery(conn, "select * from wiki_raw 
-           where id between 8 and 50")
-t <- unlist(aa)[127:173]
-cyt <- stri_extract_all_regex(t, "<ref>(.)*?</ref>")
-link <- stri_extract_all_regex(t, "\\[\\[[^:\\]]+?:[^:]+?\\]\\]")
+           where id between 1 and 500")
+t <- aa$text
+#cyt <- stri_extract_all_regex(t, "<ref>(.)*?</ref>")
+#link <- stri_extract_all_regex(t, "\\[\\[[^:\\s\\]]+?\\]\\]")
+#link <- stri_extract_all_regex(t, "\\[\\[[^:\\]]+?:\\s.+?\\]\\]")
+#unlist(lapply(link, length))
+link <- stri_extract_all_regex(t, "\\[\\[([^:\\]]+?:\\s(.)+?|[^:\\]]+?)\\]\\]")
 
-link2 <- stri_extract_all_regex(t, "\\[\\[[^:\\]]+?:\\S[^:]+?\\]\\]")
-unlist(lapply(link2, length))
+not_link <- stri_extract_all_regex(t, "\\[\\[[^:\\]]+?:\\S[^:]+?\\]\\]")
+
+
+
 klamr <- stri_extract_all_regex(t, "\\{\\{(.)*?\\}\\}")
 troj <- stri_extract_all_regex(t, "<.*?>(.)*?<.*?>")
+opcj <- unique(unlist(stri_extract_all_regex(unlist(troj), "</(.)+?>")))
 
-dbGetQuery(conn, "select count(1) as ile from wiki_raw where ns=0")
+#dbGetQuery(conn, "select count(1) as ile from wiki_raw where ns=0")
 
 dbDisconnect(conn)
 

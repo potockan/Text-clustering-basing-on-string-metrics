a871157df7395cc3b3bc9327d7e3462c1028d49a
potockan <potockan@student.mini.pw.edu.pl>
2014-12-28 18:00:22 +0100

    clean up

--- R/drop_wiki_fit.R
+++ R/drop_wiki_fit.R
@@ -1,23 +1,8 @@
 #!/usr/bin/Rscript --vanilla
 
-#install.packages("RSQLite")
-library(RSQLite)
-source("./R/db_exec.R")
+
 ### Delete raw text database ###
 
-file.remove("./Data/DataBase/wiki.sqlite")
-# con <- dbConnect(SQLite(), dbname = "./Data/DataBase/wiki.sqlite")
-# 
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_word_freq;")
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_word;")
-# # dbExecQuery(con, "DROP TABLE IF EXISTS wiki_curly;")
-# # dbExecQuery(con, "DROP TABLE IF EXISTS wiki_tag;")
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_redirect;")
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_link;")
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_category_text;")
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_category_name;")
-# dbExecQuery(con, "DROP TABLE IF EXISTS wiki_page;")
+#commented in case of an error
+#file.remove("./Data/DataBase/wiki.sqlite")
 
-#dbListTables(con)
-
-#dbDisconnect(con)

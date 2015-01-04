8ab1f352bfa76bade843cd4cf9f8e0169b5476a5
potockan <potockan@student.mini.pw.edu.pl>
2015-01-04 18:19:16 +0100

    clustering attempts

--- R/word_analysis.R
+++ R/word_analysis.R
@@ -1,4 +1,4 @@
-install.packages('stringdist')
+#install.packages('stringdist')
 library(stringdist)
 library(stringi)
 
@@ -8,12 +8,12 @@ n_words <- nrow(word_stat)
 n_once <- sum(word_stat$word_cnt==1)/n_words*100
 
 n_articles <- readRDS("./Data//RObjects//n_articles.rds")
-
-word_stat_all <- 
-  readRDS("./Data/RObjects/words_cnt_all.rds")
-n_words_all <- nrow(word_stat_all)
-n_once_all <- sum(word_stat_all$word_cnt==1)/n_words_all*100
-
+# 
+# word_stat_all <- 
+#   readRDS("./Data/RObjects/words_cnt_all.rds")
+# n_words_all <- nrow(word_stat_all)
+# n_once_all <- sum(word_stat_all$word_cnt==1)/n_words_all*100
+#
 # set.seed(208)
 # sample(word_stat$word[word_stat$word_cnt==1], 10)
 # 
@@ -29,38 +29,102 @@ n_once_all <- sum(word_stat_all$word_cnt==1)/n_words_all*100
 stopwords <- as.vector(read.table("./Data//RObjects//stopwords2.txt", sep="")[,1])
 
 words <- c(word=setdiff(word_stat$word, 
-                                 c(stopwords, word_stat$word[2806173:2806765])))
+                        c(stopwords, word_stat$word[2806173:2806765])))
 words <- words[-(which(stri_length(words)==1))]
 #word_no_stp <- merge(word_stat, words)
 n_no_stp <- length(words)
 
 ############ clustering ############
 
-set.seed(125)
-sam <- sample(1:n_no_stp, 20)
-
-dist_lv <- stringdistmatrix(words[sam], words, method = 'lv')
-dist_lv2 <- t(dist_lv)
-mean(dist_lv)
-max(dist_lv)
-#max(stri_length(words[sam]))
-
-
-kmeans_lv <- kmeans(dist_lv2, centers=dist_lv2[sam,], iter.max=3)
-simmilar <- which(dist_lv[1,]<=6)
-
-unsimmilar <- numeric(20)
+ble <- stringdistmatrix(words[sam], words[sam], method = 'lv')
+s  <- numeric(20)
 for(i in 1:20)
-unsimmilar[i] <- sum(which(dist_lv[1])>6)
-sum(simmilar)
-
-w <- dist_lv2[,1:3]
-kmeans_lv <- kmeans(w, centers=20)
-unique(w)
+  s[i] <- sum(ble[i,])
+s <- s/19
+mean(s)
 
 
+set.seed(128)
+dimm <- 3
+sam <- sample(1:n_no_stp, dimm)
+nclust <- 20
+dist_lv2 <- stringdistmatrix(words, words[sam], method = 'lv')
+kmeans_lv <- kmeans(dist_lv2, centers=nclust, iter.max=20)
 
 
+
+dist_lv3 <- numeric(0)
+for(i in 1:20)
+  dist_lv3 <- c(dist_lv3, 
+                stringdist(words, words[sam[i]], method = 'lv'))
+dist_lv3 <- matrix(dist_lv3, ncol=20, byrow=FALSE)
+
+
+
+
+# simmilar <- which(dist_lv[1,]<=6)
+# 
+# unsimmilar <- numeric(20)
+# for(i in 1:20)
+#   unsimmilar[i] <- sum(which(dist_lv[1])>6)
+# sum(simmilar)
+# 
+# w <- dist_lv2[,1:10]
+# kmeans_lv <- kmeans(w, centers=20)
+# unique(w)
+
+euc_dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
+clust_means <- numeric(nclust)
+for(j in 1:nclust){
+  cl <- which(kmeans_lv$cluster==j)
+  coor <- kmeans_lv$centers[j,]
+
+  min_euc <- c(euc_dist(coor, dist_lv2[cl[1],]), 1)
+  for(i in 2:length(cl)){
+    euc <- euc_dist(coor, dist_lv2[cl[i],])
+    if(euc<min_euc[1]){
+      min_euc <- c(min(min_euc, euc), cl[i])
+    }
+    
+  }
+  clust_means[j] <- min_euc[2]
+}
+
+
+
+
+
+######### word order #########
+words_order <- character(n_no_stp)
+index_in <- numeric(n_no_stp)
+for(i in 1:n_no_stp){
+  a <- stringdist(words[-1], words[1])
+}
+
+
+
+
+
+
+
+
+
+
+
+########### two nearest points ##########
+cl_1 <- which(kmeans_lv$cluster==1)
+coor_1 <- kmeans_lv$centers[1,]
+euc_dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
+min_euc <- c(euc_dist(coor_1, dist_lv2[cl_1[1],]), 1)
+min_euc2 <- min_euc
+for(i in 2:length(cl_1)){
+  euc <- euc_dist(coor_1, dist_lv2[cl_1[i],])
+  if(euc<min_euc[1]){
+    min_euc2 <- min_euc
+    min_euc <- c(min(min_euc, euc), cl_1[i])
+  }
+  
+}
 
 
 

ab94fc7afc4a7a86767de297bfdcb54328992ae6
potockan <potockan@student.mini.pw.edu.pl>
2015-02-26 20:25:49 +0100

    clustering using LanguageTool

--- R/langtool_tag_analysis.R
+++ R/langtool_tag_analysis.R
@@ -1,4 +1,3 @@
-
 library(RSQLite)
 library(stringi)
 library(compiler)
@@ -14,37 +13,38 @@ prepare_string <- cmpfun(function(str) {
 
 con <- dbConnect(SQLite(), dbname = "/dragon/Text-clustering-basing-on-string-metrics/Data/DataBase/wiki.sqlite")
 
-dbExecQuery(con, "CREATE TABLE IF NOT EXISTS wiki_hunspell_clust (
-  id_word INTEGER NOT NULL,
-  id_stem_word INTEGER NOT NULL,
-  FOREIGN KEY (id_word) REFERENCES wiki_word(id),
-  FOREIGN KEY (id_stem_word) REFERENCES wiki_word(id)
+dbExecQuery(con, "CREATE TABLE IF NOT EXISTS wiki_langtools_tag_clust (
+            id_word INTEGER NOT NULL,
+            id_stem_word INTEGER NOT NULL,
+            FOREIGN KEY (id_word) REFERENCES wiki_word(id),
+            FOREIGN KEY (id_stem_word) REFERENCES wiki_word(id)
 );")
 
-dbExecQuery(con, "create table tmp_hunspell (
+dbExecQuery(con, "create table tmp_tag (
             word VARCHAR(256) NOT NULL,
             stem_word VARCHAR(256) NOT NULL
-            )")
+)")
 
-f <- file("/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/words_analiza_morfologiczna-sorted.txt", "r")
-while (length(txt <- readLines(con = f, n=8192)) > 0) {
-  
-  txt <- txt[which(stri_length(txt)>0)]
-  
-  txt_split <- lapply(stri_split_fixed(txt, " "), function(txt){
-    p <- unlist(stri_split_fixed(txt, "st:") )
-    p <- p[which(stri_length(p)>0 & p!="fl")]
-    n <- length(p)
-    if(n==1)
-      p[2] <- ""
-    p[1:2]
+
+f <- file("/home/natalia/LanguageTool-2.8/words_tag.txt", "r")
+
+while (length(out <- readLines(con = f, n=3)) > 0) {
+  out1 <- 
+    stri_split_fixed(unlist(stri_split_fixed(out, "]", omit_empty = TRUE)), "[", omit_empty = TRUE)
+  out2 <- lapply(out1, function(x){
+    
+    #   stri_trans_tolower(
+    #     stri_extract_all_regex(x, ",([^/]+)/", omit_no_match = TRUE)
+    #     )
+    p1 <- stri_trim_both(unlist(stri_split_fixed(x, "<S> ", omit_empty = TRUE))[1])
+    p2 <- stri_trans_tolower(unlist(stri_split_fixed(x[2], "/", omit_empty = TRUE))[1])
+    c(p1, ifelse(length(p2)==0, p1, p2))
     
   })
-  txt_split
+ 
   
-  m1 <- matrix(unlist(txt_split), ncol = 2, byrow = TRUE)
-  m1 <- m1[-which(duplicated(m1[,1])),]
-  
+  m1 <- matrix(unlist(out2), ncol = 2, byrow = TRUE)
+  m1 <- m1[-which(is.na(m1[,1])),]
   
   to_insert <- sprintf("(%s, %s)", prepare_string(m1[,1]), prepare_string(m1[,2]))
   to_insert <- split(to_insert, rep(1:ceiling(length(to_insert)/500), 
@@ -52,7 +52,7 @@ while (length(txt <- readLines(con = f, n=8192)) > 0) {
   
   lapply(to_insert, function(to_insert) {
     dbExecQuery(con, sprintf("INSERT into tmp_hunspell(word, stem_word)
-                    values %s", stri_flatten(to_insert, collapse=",")))
+                             values %s", stri_flatten(to_insert, collapse=",")))
   })
   
   
@@ -78,3 +78,4 @@ dbExecQuery(con, "drop table tmp_hunspell")
 dbGetQuery(con, "select count (distinct id_stem_word) from wiki_hunspell_clust")
 
 dbDisconnect(con)
+

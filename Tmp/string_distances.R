2847b9febc44eb11d56e5d23daa2f51392446ef4
npotocka <natalia.potocka@grupawp.pl>
2015-08-23 15:03:06 +0200

    update

--- Tmp/string_distances.R
+++ Tmp/string_distances.R
@@ -18,3 +18,33 @@ stringdist('ab', 'acb', method = 'osa')
 
 stringdist('ba', 'acb', method = 'dl')
 
+
+
+
+
+stringdist('krolik', 'kolor', method = 'hamming')
+stringdist('krolik', 'kolor', method = 'lcs')
+stringdist('krolik', 'kolor', method = 'lv')
+stringdist('krolik', 'kolor', method = 'lv', weight = c(0.1, 1, 0.3))
+stringdist('krolik', 'kolor', method = 'lv', weight = c(1, 0.1, 0.3))
+stringdist('kolor', 'krolik', method = 'lv', weight = c(0.1, 1, 0.3))
+
+stringdist('krolik', 'krol', method = 'osa')
+stringdist('krol', 'kolor', method = 'osa')
+stringdist('krolik', 'kolor', method = 'osa')
+
+stringdist('palec', 'palace', method = 'osa')
+stringdist('palec', 'palce', method = 'osa')
+stringdist('palce', 'palace', method = 'osa')
+
+stringdist('ba', 'acb', method = 'osa')
+stringdist('ba', 'ab', method = 'osa')
+stringdist('ab', 'acb', method = 'osa')
+
+stringdist('ba', 'acb', method = 'dl')
+stringdist('krolik', 'kolor', method = 'dl')
+
+
+
+
+

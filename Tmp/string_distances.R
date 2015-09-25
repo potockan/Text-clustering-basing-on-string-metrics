eb06ffe0927572c30547c39bdbc27d71ae13087e
npotocka <natalia.potocka@grupawp.pl>
2015-09-25 12:58:51 +0200

    update

--- Tmp/string_distances.R
+++ Tmp/string_distances.R
@@ -1,6 +1,6 @@
 library("stringdist")
 
-stringdist('koza', 'foka', method = 'hamming')
+stringdist('kozak', 'foczka', method = 'hamming')
 stringdist('koza', 'foka', method = 'lcs')
 stringdist('koza', 'foka', method = 'lv')
 stringdist('koza', 'foka', method = 'lv', weight = c(0.1, 1, 0.3))
@@ -18,16 +18,20 @@ stringdist('ab', 'acb', method = 'osa')
 
 stringdist('ba', 'acb', method = 'dl')
 
-
-
+stringdist('koza', 'foka', method = 'lcs')
+stringdist('kozak', 'foczka', method = 'lcs')
+stringdist('kozak', 'foczka', method = 'lv')
+stringdist('kozak', 'foczka', method = 'osa')
+stringdist('kozak', 'foczka', method = 'dl')
 
 
 stringdist('krolik', 'kolor', method = 'hamming')
 stringdist('krolik', 'kolor', method = 'lcs')
 stringdist('krolik', 'kolor', method = 'lv')
-stringdist('krolik', 'kolor', method = 'lv', weight = c(0.1, 1, 0.3))
-stringdist('krolik', 'kolor', method = 'lv', weight = c(1, 0.1, 0.3))
-stringdist('kolor', 'krolik', method = 'lv', weight = c(0.1, 1, 0.3))
+stringdist('koza', 'foka', method = 'lv', weight = c(0.1, 1, 0.3))
+stringdist('kozak', 'foczka', method = 'lv', weight = c(1, 0.2, 0.3))
+stringdist('foczka', 'kozak', method = 'lv', weight = c(0.2, 1, 0.3))
+stringdist('a', 'k', method = 'lv', weight = c(0.1, 1, 0.3)) #??????????
 
 stringdist('krolik', 'krol', method = 'osa')
 stringdist('krol', 'kolor', method = 'osa')

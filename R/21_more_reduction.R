cb641d30901e89234b0b40c0544c991a5ec944f0
potockan <potockan@student.mini.pw.edu.pl>
2015-11-19 21:07:27 +0100

    reduction - done

--- R/21_more_reduction.R
+++ R/21_more_reduction.R
@@ -222,6 +222,8 @@ for(i in 1:nrow(kategorie)){
     
 }
 
+kategorie$nowa_kat2[kategorie$nowa_kat2 == 'chleb'] <- 'potrawy według głównych składników'
+
 save(list = 'kategorie', file = "/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/categories/kategorie20151118.rda")
 load("/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/categories/kategorie20151118.rda")
 
@@ -231,13 +233,6 @@ load("/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/categories/
 
 
 
-load("/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/categories/level10.rda")
-level10 %>% select(-level1.link) -> level10
-
-for(i in names(level10)){
-  varval <- sprintf("tolower(%s)", i)
-  level10 <- level10 %>% mutate_(.dots= setNames(list(varval), i))
-}
 
 nieskl <- kategorie$name_new[kategorie$nowa_kat == ""]
 
@@ -276,34 +271,88 @@ nieskl$level1.name[19] <- 'kategorie według położenia geograficznego'
 nieskl$level1.name[c(21,22,23,25,26,27,39:48,56,58:61, 63, 67,72, 74:76, 78,85,89,92:96, 100, 101, 103:111,114,120,120)] <- 'kategorie według położenia geograficznego'
 
 
-geo <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), "państw|pojezierze|warszawa|krajobraz|kamienic|rynek|północ|nizina|linia|wodna|prl|gatun|strażni|region|nornik|kraj|ocean|gdańsk|powiat|alp|rio|azja|pasmo|przełęcz|hrabstw|śląsk|dzielnic|tatr|beskid|szczyt|wysp|administr|potok|rzek|wsi|miejscowoś|gmin|jezior|miast|porty|dystrykt|gór"))
+geo <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), "raczy|sydney|pobrzeże|rudawy|cmentarz|skałki|wody|warszaw|krakow|wyżyn|słowack|pienin|wodne|rejony buriacji|nizin|wop|praga|państw|pojezierze|warszawa|krajobraz|kamienic|rynek|północ|nizina|linia|wodna|prl|gatun|strażni|region|nornik|kraj|ocean|gdańsk|powiat|alp|rio|azja|pasmo|przełęcz|hrabstw|śląsk|dzielnic|tatr|beskid|szczyt|wysp|administr|potok|rzek|wsi|miejscowoś|gmin|jezior|miast|porty|dystrykt|gór"))
 nieskl$level1.name[geo] <- 'kategorie według położenia geograficznego'
 
-sw <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'orunia|zakonni|eparchi|kości|diecezj|świątynie|chrystus|wezwani|zbor|jehow|parafi|święt'))
+sw <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'kanonu|kurii|katolic|orunia|zakonni|eparchi|kości|diecezj|świątynie|chrystus|wezwani|zbor|jehow|parafi|święt'))
 nieskl$level1.name[sw] <- 'religie'
 
-woj <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'staroż|senat|sejm|żołnierz|polowe|herb|pistole|batalion|arm|wojsk|pułk|dywizj|altyleri|kawaleri|piechot'))
+woj <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'organizacje|ordynacje|druży|władz|rządy|order|sił powietrznych|fantastycz|staroż|senat|sejm|żołnierz|polowe|herb|pistole|batalion|arm|wojsk|pułk|dywizj|altyleri|kawaleri|piechot'))
 nieskl$level1.name[woj] <- 'społeczeństwo'
 
 
-sp <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'żeglug|siatkar|stadion|sezon|league|piłka|formuł|żużel|sport|mistrzostwa|świat|puchar'))
+sp <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'memoria|żagl|żużlu|żeglug|siatkar|stadion|sezon|league|piłka|formuł|żużel|sport|mistrzostwa|świat|puchar'))
 nieskl$level1.name[sp] <- 'sport'
 
-kult <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'staroż|krętorog|azolidy|głupkow|parti|mięczak|ślimak|średniow|zębowc|mroczko|chińs|roślin|gorzyk|kawa|azynany|makako|ptak'))
-nieskl$level1.name[kult] <- 'klasyfikacja nauk'
+eak <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'panther|amonit|diaz|jaszczur|łaszo|ryby|gady|bławatniki|synaps|ssaki|krzepni|morfogenet|kształtne|szczuroskoczkowate|argiolestidae|krętorog|azolidy|głupkow|mięczak|ślimak|zębowc|mroczko|roślin|gorzyk|kawa|azynany|makako|ptak'))
+nieskl$level1.name[eak] <- 'klasyfikacja nauk'
 
-szt <-  which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'film|oper|album|artys|artyś|piosenk|singl|sztuka|koncert|zespoł|zespół'))
+kult <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'teloschistales|brygad|orderem|staroż|parti|średniow|chińs'))
+nieskl$level1.name[kult] <- 'etnologia i antropologia kulturowa'
+
+szt <-  which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'adelajda|scena|romek|film|oper|album|artys|artyś|piosenk|singl|sztuka|koncert|zespoł|zespół'))
 nieskl$level1.name[szt] <- 'sztuka'
 
-tech <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'lokomot|ubunt|licyt|silnik'))
+tech <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'autobus|proceso|linuks|kolej|motorola|lokomot|ubunt|licyt|silnik'))
 nieskl$level1.name[tech] <- 'technika'
 
-str <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'siog'))
+str <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'siog|nts'))
 nieskl$level1.name[str] <- 'strony przeglądowe'
 
 para <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'paraps|paranau'))
 nieskl$level1.name[para] <- 'paranauka'
 
+trylo <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'biografie|trylobit'))
+nieskl$level1.name[trylo] <- 'biografie według epok i okresów historycznych'
+
+bio <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'encyklopedii'))
+nieskl$level1.name[bio] <- 'biografie według profesji'
+
+info <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'ukryte kategorie|kategorie'))
+nieskl$level1.name[info] <- 'informacja'
+
+potr <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'potrawy'))
+nieskl$level1.name[potr] <- 'potrawy według głównych składników'
+
+zon <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'żony|imiona'))
+nieskl$level1.name[zon] <- 'kobieta'
+
+histsp <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'lotnisk|życie|rzeczypospolit|ławeczki|polityka|przedrozbiorowej|orientacje'))
+nieskl$level1.name[histsp] <- 'historia społeczna'
+
+mn <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'transmisj'))
+nieskl$level1.name[mn] <- 'metodologia nauki'
+
+wie <- which(stri_detect_regex(stri_trans_tolower(nieskl$name), 'wiedźm'))
+nieskl$level1.name[wie] <- 'fikcja'
+
+nieskl$level1.name %>% setdiff(kategorie$nowa_kat2) -> reszta
+
+nieskl$level1.name[nieskl$level1.name %in% reszta] <- 'klasyfikacja nauk'
 
 nieskl$name[is.na(nieskl$level1.name)]
 sum(is.na(nieskl$level1.name))
+
+
+
+
+kategorie %>% left_join(nieskl, by = c('name_new' = 'name')) -> kategorie
+kategorie$nowa_kat2 <- ifelse(!is.na(kategorie$level1.name) & kategorie$nowa_kat2 == '', kategorie$level1.name, kategorie$nowa_kat2)
+
+kategorie$name_new[kategorie$nowa_kat2 == 'klasyfikacja nauk'] -> klnk
+
+kategorie$nowa_kat2[kategorie$name_new %in% klnk[c(1,2,4:16, 19:22, 26:31, 33:40, 42:49, 51, 53, 55:57, 59:60, 62:64, 67:70, 72:84, 86:87, 89:93, 95:108, 110, 112:115, 118:125, 127:129,131,132,134:145,147:150,152:158,160:161,163:165, 169,170, 173:181, 184:204,206:216,218:222, 224, 225)]] <- 
+  'nauki przyrodnicze'
+
+kategorie %>% names()
+
+kategorie <- kategorie[,c(1,2,6)]
+
+kategorie$nowa_kat2 %>% unique() %>% sort() -> kat_unq
+kat_unq <- data.frame(nowa_kat2 = kat_unq, id_new2 = 1:length(kat_unq))
+kategorie %>% left_join(kat_unq) -> kategorie
+
+save(list = 'kategorie', file = "/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/categories/kategorie20151119_ost.rda")
+load("/dragon/Text-clustering-basing-on-string-metrics/Data/RObjects/categories/kategorie20151119_ost.rda")
+
+kategorie %>% group_by(id_new2) %>% count(id_new2) ->aa

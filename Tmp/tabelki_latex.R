805474dedb940baf7bf0e2dcd67a83cbd90a652f
potockan <potockan@student.mini.pw.edu.pl>
2015-12-14 20:54:11 +0100

    update

--- Tmp/tabelki_latex.R
+++ Tmp/tabelki_latex.R
@@ -20,5 +20,62 @@ xtable::xtable(aa)
 # po jednym na rozne licznosci zbioru, 
 # po 3 na zbior.
 # takich wykresow mozna zrobic od 3 do 6
+library(RSQLite)
+library(tidyr)
+
+con <- dbConnect(SQLite(), dbname = "/dragon/Text-clustering-basing-on-string-metrics/Data/DataBase/wiki.sqlite")
+
+dbGetQuery(con, "select distinct id_stem_word from wiki_hunspell_clust2 limit 20")[,1] -> stem_words
+
+skupienia <- dbGetQuery(con, sprintf("select *
+           from wiki_hunspell_clust2 a
+           join 
+           wiki_word b
+           on a.id_stem_word = b.id
+           join 
+           wiki_word c
+           on a.id_word = c.id
+           where a.id_stem_word in (%s)",
+                        stri_flatten(stem_words, collapse = ", ")))
+names(skupienia)[c(5,6)] <- c("id2", "word2")
+skupienia <- skupienia %>% arrange(id_stem_word)
+
+skupienia <- skupienia %>% filter(!(word %in% c("de", "nad", "np", "roku", "też", "the", "który", "wiele", "miasta", "jednym", "wśród")))
+
+skupienia <- skupienia %>% select(word, word2)
+skupienia2 <- skupienia %>% spread(word, word2)
+
+bb <- cumsum(c(0,apply(skupienia2, 2, function(x) sum(!is.na(x)))))
+l <- max(bb)
+
+cc <- list()
+for(i in 1:(length(bb)-1)){
+  cc[[i]] <- c(skupienia2[(bb[i]+1):bb[i+1],i], rep(NA, 31))[1:31]
+  
+}
+
+skupienia3 <- data.frame(cc)
+names(skupienia3) <- names(skupienia2)
+
+for(i in 1:ncol(skupienia3)){
+  skupienia3[,i] <- as.character(skupienia3[,i])
+  skupienia3[is.na(skupienia3[,i]),i] <- ""
+}
+
+aa <- xtable::xtable(skupienia3)
+print(aa,  include.rownames = FALSE)
+
+
+#####
+liczn <- dbGetQuery(con, "select count(1) from wiki_hunspell_clust2 group by id_stem_word")
+
+liczn <- summary(liczn$`count(1)`)
+names(liczn)
+liczn2 <- data.frame(t(matrix(liczn)))
+names(liczn2) <- names(liczn)
+xtable::xtable(liczn2, digits = 0)
+
+
+dbDisconnect(con)
 
 
